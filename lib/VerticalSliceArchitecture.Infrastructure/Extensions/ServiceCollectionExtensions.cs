cf7f7993004d92c7ab39aa0d630e7044e7d698ca
Neyzu <furyax13@gmail.com>
2026-03-05 14:53:52 +0100

    feat: setting up infrastructure services

---
+++ lib/VerticalSliceArchitecture.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VerticalSliceArchitecture.Infrastructure.Persistence.InMemory;
+using VerticalSliceArchitecture.Infrastructure.Persistence.Seeding;
+
+namespace VerticalSliceArchitecture.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    extension (IServiceCollection services)
+    {
+        public IServiceCollection AddInfrastructure(Action<IServiceProvider, DbContextOptionsBuilder>? configure = null) =>
+            services
+                .AddDbContextFactory<InMemoryDbContext>((sp, o) => configure?.Invoke(sp, o));
+    }
+}
