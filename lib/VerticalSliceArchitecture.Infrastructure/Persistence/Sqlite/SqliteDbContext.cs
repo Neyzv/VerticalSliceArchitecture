924a428559c92e59375c9429f9ba8f6885bcb1fb
Neyzu <furyax13@gmail.com>
2026-03-06 11:39:37 +0100

    feat: adding sqlite db context

---
+++ lib/VerticalSliceArchitecture.Infrastructure/Persistence/Sqlite/SqliteDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerticalSliceArchitecture.Infrastructure.Persistence.Seeding;
+
+namespace VerticalSliceArchitecture.Infrastructure.Persistence.Sqlite;
+
+public sealed class SqliteDbContext(DbContextOptions<SqliteDbContext> options, IEnumerable<ISeeder<SqliteDbContext>> seeders)
+    : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SqliteDbContext).Assembly);
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSeeding(Seed)
+            .UseAsyncSeeding(SeedAsync);
+    }
+
+    private void Seed(DbContext context, bool migrationApplied)
+    {
+        if (context is not SqliteDbContext sqliteDbContext)
+            return;
+        
+        foreach (var seeder in seeders.Where(x => x.ShouldBeApplied(sqliteDbContext)))
+            seeder.Seed(sqliteDbContext);
+    }
+
+    private async Task SeedAsync(DbContext context, bool migrationApplied, CancellationToken token)
+    {
+        if (context is not SqliteDbContext sqliteDbContext)
+            return;
+        
+        var tasks = new List<Task>();
+
+        foreach (var seeder in seeders)
+        {
+            if (!await seeder.ShouldBeAppliedAsync(sqliteDbContext, token).ConfigureAwait(false))
+                continue;
+
+            tasks.Add(seeder.SeedAsync(sqliteDbContext, token));
+        }
+
+        await Task.WhenAll(tasks).ConfigureAwait(false);
+    }
+}
