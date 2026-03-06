4f6ec998cc3f6eb93627587d4ae546bc693b232f
Neyzu <furyax13@gmail.com>
2026-03-06 11:52:09 +0100

    feat: adding song model and configuration

---
+++ lib/VerticalSliceArchitecture.Infrastructure/Persistence/Seeding/Entities/SongEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace VerticalSliceArchitecture.Infrastructure.Persistence.Seeding.Entities;
+
+public sealed class SongEntity
+{
+    public Guid Id { get; set; }
+    
+    public required string Title { get; set; }
+    
+    public TimeSpan Duration { get; set; }
+}
