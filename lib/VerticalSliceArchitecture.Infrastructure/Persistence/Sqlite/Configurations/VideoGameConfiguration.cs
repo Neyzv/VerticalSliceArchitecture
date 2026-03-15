d002a40cbf854b5ef11076f15bd3e7cb2f9ca740
Neyzu <furyax13@gmail.com>
2026-03-15 16:50:46 +0100

    fix: fixing index on video game entity

--- lib/VerticalSliceArchitecture.Infrastructure/Persistence/Sqlite/Configurations/VideoGameConfiguration.cs
+++ lib/VerticalSliceArchitecture.Infrastructure/Persistence/Sqlite/Configurations/VideoGameConfiguration.cs
@@ -29,7 +29,7 @@ public sealed class VideoGameConfiguration
             .IsRequired();
         builder
             .HasIndex(static v => v.Genre)
-            .IsUnique();
+            .IsUnique(false);
         
         builder
             .Property(static v => v.Description)
