6bc8ef6678ef6ca36630f4b6a87a3b1bfb3d1af0
Neyzu <furyax13@gmail.com>
2026-03-21 17:07:01 +0100

    feat: cleaning up project + setup of facet

---
+++ src/VerticalSliceArchitecture.Api/Features/Movies/GetMoviesFeature.cs
@@ -0,0 +1,19 @@
+﻿using MediaThor;
+using VerticalSliceArchitecture.Api.Models.Movies;
+
+namespace VerticalSliceArchitecture.Api.Features.Movies;
+
+public static class GetMoviesFeature
+{
+    public sealed record GetMoviesQuery()
+        : IStreamRequest<GetMovieDto>;
+
+    public sealed class GetMoviesHandler
+        : IStreamRequestHandler<GetMoviesQuery, GetMovieDto>
+    {
+        public IAsyncEnumerable<GetMovieDto> HandleAsync(GetMoviesQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/VerticalSliceArchitecture.Api/Models/Movies/GetMovieDto.cs
@@ -0,0 +1,20 @@
+﻿using Facet;
+using Facet.Mapping;
+using VerticalSliceArchitecture.Infrastructure.Persistence.InMemory.Entities;
+
+namespace VerticalSliceArchitecture.Api.Models.Movies;
+
+[Facet(typeof(MovieEntity), Include = [nameof(MovieEntity.Title), nameof(MovieEntity.Description)])]
+public sealed partial record GetMovieDto
+{
+    public int YearOfRelease { get; set; }
+}
+
+public sealed class GetMovieDtoConfiguration
+    : IFacetMapConfiguration<MovieEntity, GetMovieDto>
+{
+    public static void Map(MovieEntity source, GetMovieDto target)
+    {
+        target.YearOfRelease = source.ReleaseDate.Year;
+    }
+}
