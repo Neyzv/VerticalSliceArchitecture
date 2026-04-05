73d226a12eea0ec26694e0871a50a6bfd3573aac
Neyzu <furyax13@gmail.com>
2026-04-05 19:49:48 +0200

    feat(author): restricting the length of the author name

--- lib/VerticalSliceArchitecture.Infrastructure.InMemory/Configurations/AuthorConfiguration.cs
+++ lib/VerticalSliceArchitecture.Infrastructure.InMemory/Configurations/AuthorConfiguration.cs
@@ -14,6 +14,7 @@ public sealed class AuthorConfiguration
         builder.HasKey(static x => x.Id);
 
         builder.Property(static x => x.Name)
+            .HasMaxLength(80)
             .IsRequired();
         builder.HasIndex(static x => x.Name)
             .IsUnique(false);
